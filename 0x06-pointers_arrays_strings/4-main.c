14377b252a96b2482c1e77b9a3281a9fc28a209e
Obidokun Tunji <tunjiobidokun@gmail.com>
2023-03-02 14:37:57 -0800

    a function that changes all lowercase letters of a string to uppercase.

--- 0x06-pointers_arrays_strings/4-main.c
+++ 0x06-pointers_arrays_strings/4-main.c
@@ -1,24 +1,41 @@
 #include "main.h"
+#include <stdio.h>
+
 /**
- * reverse_array - reverses an array
+ * main - check the code
  * @a: an array of integers
- * @n: number of elements of an array
+ * @n: the number of elements to swap
  *
+ * Return: nothing.
  */
-void reverse_array(int *a, int n)
+void print_array(int *a, int n)
 {
-	int i;
-	int temp;
+    int i;
 
-	i = 0;
-	n = n - 1;
+    i = 0;
+    while (i < n)
+    {
+        if (i != 0)
+        {
+            printf(", ");
+        }
+        printf("%d", a[i]);
+        i++;
+    }
+    printf("\n");
+}
 
-	while (i < n)
-	{
-		temp = a[i];
-		a[i] = a[n];
-		a[n] = temp;
-		i++;
-		n--;
-	}
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int a[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 98, 1024, 1337};
+
+    print_array(a, sizeof(a) / sizeof(int));
+    reverse_array(a, sizeof(a) / sizeof(int));
+    print_array(a, sizeof(a) / sizeof(int));
+    return (0);
 }
